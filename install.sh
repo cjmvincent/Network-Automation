b25d6b7a10761f750f8b0d8499b7771390821ca8
Charles Vincent <cjmvincent@outlook.com>
2023-01-19 10:00:16 -0500

    clean up comments

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 # to get started simply run:
 # bash -c "$(wget -qO - https://raw.githubusercontent.com/cjmvincent/Network-Automation/main/install.sh)"
 
-
+# just to make sure all packages and such are fresh
 sudo apt update
 sudo apt upgrade -y
 
 
-# add required repositories
+# add required repositories for the packages and utilities I want to install
 sudo add-apt-repository --yes universe
 sudo add-apt-repository --yes ppa:ansible/ansible
 sudo add-apt-repository --yes ppa:deadsnakes/ppa
@@ -16,34 +16,34 @@ sudo apt upgrade -y
 
 
 # install libs, dependencies, and apps
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev net-tools python3-pip python3-yaml python3-jinja2 python3-httplib2 python3-netaddr python3-paramiko python3-pkg-resources libffi-dev python3-all-dev python3-mysqldb python3-selinux python3-boto python3 python3-pip ansible wget software-properties-common git-core ansible -y
+sudo apt install build-essential net-tools python3.8 ansible oftware-properties-common git-core ansible gufw -y
 
 sudo apt update
 sudo apt upgrade -y
 
+
 pip install paramiko netmiko napalm pywinrm ansible-pylibssh
 
 
-# clone repo to ansible directory
+# clone ansilbe repo to ansible directory
 cd /etc/ansible
 sudo rm * -rf
 sudo git clone https://github.com/cjmvincent/Network-Automation.git .
 
 
 # install desired ansible collections
-ansible-galaxy collection install cisco.ios
+#ansible-galaxy collection install cisco.ios
 #ansible-galaxy collection install paloaltonetworks.panos
 #ansible-galaxy collection install arubanetworks.aoscx
 #ansible-galaxy collection install arubanetworks.aos_switch
 
 
 # install and create site (instance) of checkmk
-cd /tmp/
-wget https://download.checkmk.com/checkmk/1.6.0p27/check-mk-raw-1.6.0p27_0.focal_amd64.deb
-sudo apt install -y ./check-mk-raw-1.6.0p27_0.focal_amd64.deb
-
-sudo omd create your_site_name > ~/Desktop/checkmkcreds.txt
-sudo omd start your_site_name
+#cd /tmp/
+#wget https://download.checkmk.com/checkmk/1.6.0p27/check-mk-raw-1.6.0p27_0.focal_amd64.deb
+#sudo apt install -y ./check-mk-raw-1.6.0p27_0.focal_amd64.deb
+#sudo omd create your_site_name > ~/Desktop/checkmkcreds.txt
+#sudo omd start your_site_name
 
 
 # configure firewall
