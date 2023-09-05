845a03fddc24dda62cbd86ae21e95ec47f9145ba
Charles Vincent <cjmvincent@outlook.com>
2023-09-05 09:05:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -76,4 +76,4 @@ sudo systemctl enable --now docker
 
 
 # install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
