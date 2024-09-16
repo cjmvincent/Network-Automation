49f03a914fbe927062eeebc27a5a177e453ac620
Charles Vincent <cjmvincent@outlook.com>
2024-09-16 08:54:34 -0400

    add ohmyzsh plugins

--- install.sh
+++ install.sh
@@ -118,12 +118,12 @@ fi
 # install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# install a couple of zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # change ohmyzsh theme to powerlevel10k
 #sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' $HOME/.zshrc
-
-# # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-# rm -rf $HOME/.zshrc
-# ln -s .zshrc $HOME/.zshrc%          
 
 
 ###############################################################################
@@ -152,5 +152,4 @@ sudo ufw allow 4440/tcp
 
 git clone https://github.com/cjmvincent/dotfiles.git "~/.dotfiles"
 cd "~/.dotfiles"
-chmod +x install
 ./install
